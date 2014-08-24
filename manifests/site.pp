5f3d642a37c30becdc7a7adc7940a1049f5eab71
Danny Arnold <despairblue@skeil.de>
2014-08-24 17:17:43 +0200

    Save time of last run in tmp file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,12 @@
 File { backup => true }
 Exec { path => ['/bin', '/usr/bin', '/usr/local/bin'] }
 
+file { '/tmp/puppet.lastrun':
+  ensure  => file,
+  content => inline_template("<%= Time.now %>\n"),
+  backup  => false,
+}
+
 node 'serenity' {
   include archlinux
   include ssh
